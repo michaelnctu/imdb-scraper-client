7b9a934ddd1d83423130b73c550c3adbb529d0a3
MIchael Huang <michaelnctu@gmail.com>
2020-11-09 12:24:27 +0800

    sectionHtml done

--- movie.js
+++ movie.js
@@ -7,5 +7,25 @@ function getMovie(imdbID) {
     .then(res => res.json());
 }
 
+function showMovie(movie) {
+  const section = document.createElement('section')
+  main.appendChild(section)
+
+  section.outerHTML = `
+    <section class="row">
+      <h1 class="text-center">${movie.title}</h1>
+      <div class="col-sm-12">
+        <img src="${movie.poster}" class="img-fluid" />
+      </div>
+      <div class="col-sm-12">
+        <dl class="row">
+          ${descriptionHTML}
+        </dl>
+      </div>
+    </section>
+  `;
+
+}
+
 getMovie(imdbID)
   .then(showMovie)
